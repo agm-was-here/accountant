479c609bb7412edcd2cffba83f3925a322d71d56
agm_was_here <aswinmenon92@gmail.com>
2022-08-01 00:52:18 +0530

    adding

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import ExpenseItem from "./component/ExpenseItem";
 
 function App() {
+
   return (
     <div>
-      <h2>whats up my </h2>
+     <div Classname="index-heading"> <h2>THE ACCOUNTANT </h2></div>
+      <ExpenseItem></ExpenseItem>
       <ExpenseItem></ExpenseItem>
     </div>
   );
--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,5 +1,19 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-  return <h2>expense item</h2>;
+  const expensedate = new Date(2022, 2, 25);
+  const expensetitle = "Car insurance";
+  const expenseamount = "286.7";
+
+  return (
+    <div className="expense-item">
+      <div>{expensedate.toISOString}</div>
+      <div className="expense-item__description">
+        <h2>{expensetitle}</h2>
+      </div>
+      <div className="expense-item__price">${expenseamount}</div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
