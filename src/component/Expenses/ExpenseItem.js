a0901ecc8453802c23abb43ddd16fd9e20e91033
agm_was_here <aswinmenon92@gmail.com>
2022-08-13 15:04:56 +0530

    states

--- src/component/Expenses/ExpenseItem.js
+++ src/component/Expenses/ExpenseItem.js
@@ -3,14 +3,21 @@ import "./ExpenseItem.css";
 // import Card from "./Card"
 function ExpenseItem(props) {
 
+
+  var title =props.item;
+const clicker=()=>{
+  title ="again works";
+  console.log(title);
+}
   return (
     <div className="expense-item">
     <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.item}</h2>
+        {/* <h2>{props.item}</h2> */}
+        <h2>{title}</h2>
       </div>
       <div className="expense-item__price">${props.amount}</div>
-      <button onClick={()=>console.log("it works")}>click</button>
+      <button onClick={clicker}>click</button>
     </div>
   );
 }
