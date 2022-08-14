4963cf7f427eed4b5dc2768a467b617ad262e6d4
agm_was_here <aswinmenon92@gmail.com>
2022-08-14 23:13:15 +0530

    new

--- src/component/NewExpense/ExpenseForm.js
+++ src/component/NewExpense/ExpenseForm.js
@@ -1,16 +1,24 @@
+import "./ExpenseForm.css";
 
+function ExpenseForm() {
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input type="number" min ='0.01'/>
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input type="date" min="2021-01-01" />
+        </div>
+      </div>
+    </form>
+  );
+}
 
-
-import './ExpenseForm.css';
-
-function ExpenseForm(){
-return (
-<form>
-
-
-</form>
-)
-
-};
-
-export default ExpenseForm
+export default ExpenseForm;
