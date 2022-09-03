da9b1e85d5f946dd1f48ababfff8e755c08044ae
agm_was_here <aswinmenon92@gmail.com>
2022-09-03 22:12:14 +0530

    new addition

--- src/component/NewExpense/ExpenseForm.js
+++ src/component/NewExpense/ExpenseForm.js
@@ -14,20 +14,35 @@ function ExpenseForm() {
   function dateChangeHnadler(event) {
     seTEnteredDate(event.target.value);
   }
+  function submitHandler(event){
+    event.preventDefault();
+
+    const expenseData={
+      title:enteredTitle,
+      amount:enteredAmount,
+      date: new Date(enteredDate)
+
+    };
+
+    console.log(expenseData);
+    seTEnteredAmount('');
+    seTEnteredDate('')
+    seTEnteredTitle('')
+  }
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titlechangeHandler} />
+          <input type="text" onChange={titlechangeHandler} value={enteredTitle} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.0" onChange={amountChangeHandler} />
+          <input type="number" min="0.0" onChange={amountChangeHandler} value={enteredAmount} />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2021-01-01" onChange={dateChangeHnadler} />
+          <input type="date" min="2021-01-01" onChange={dateChangeHnadler} value={enteredDate} />
         </div>
         <div className=".new-expense__actions">
           <button type="submit">add element</button>
